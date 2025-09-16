f473c86c861c692e325e74efc08787aaa63763be
apereznwo <57961103+aperezNWO@users.noreply.github.com>
2025-09-16 02:07:08 +0000

    codesandbox update

--- web_project/settings.py
+++ web_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-b$1j7b0_*_v9ckp%ha!-*$q=dv2))6ksq^onrdnvb78uedzuxb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['vjtyrs-8000.csb.app','localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['sp223k-8000.csb.app', 'localhost', '127.0.0.1']
 
 # Application definition
 
@@ -85,10 +85,10 @@ DATABASES = {
         'PASSWORD': 'aperezNWO_SQLLogin_1',
         'HOST': 'webapiangulardemo.mssql.somee.com',
         'PORT': '1433'
-        },
-        'OPTIONS': {
-            'driver': 'ODBC Driver 17 for SQL Server',
-        },    
+    },
+    'OPTIONS': {
+        'driver': 'ODBC Driver 17 for SQL Server',
+    },
 }
 
 
