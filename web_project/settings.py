fd6c95369cb3cc0049da21806909976db861d5c7
apereznwo <57961103+aperezNWO@users.noreply.github.com>
2025-09-17 07:28:16 +0000

    codesandbox update

--- web_project/settings.py
+++ web_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-b$1j7b0_*_v9ckp%ha!-*$q=dv2))6ksq^onrdnvb78uedzuxb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['sp223k-8000.csb.app', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['sp223k-8000.csb.app', 'localhost', '127.0.0.1','apereznwo.github.io']
 
 # Application definition
 
