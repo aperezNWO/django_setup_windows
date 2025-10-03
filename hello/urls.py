b9c89162723a09190dc948d9b889bf702afc2383
apereznwo <57961103+aperezNWO@users.noreply.github.com>
2025-10-03 02:57:51 +0000

    working folder update

--- hello/urls.py
+++ hello/urls.py
@@ -4,6 +4,7 @@ from hello import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("my_view", views.my_view, name="my_view"),
+    path('health/', lambda r: JsonResponse({'status': 'ok'})),
     path('getAllLogs', views.getAllLogs,    name='getAllLogs'),
     path('getAllPersons', views.getAllPersons, name='getAllPersons'),
     path('getAllContactForms', views.getAllContactForms, name='getAllContactForms'),
