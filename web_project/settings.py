840cecd07d1eb861da7666f3aff9366e399f6666
Pablo Alejandro Perez Acosta <57961103+aperezNWO@users.noreply.github.com>
2026-09-08 05:01:56 +0000

    codespaces ubuntu update

--- web_project/settings.py
+++ web_project/settings.py
@@ -44,6 +44,7 @@ ALLOWED_HOSTS = [  'sp223k-8000.csb.app'
                    'apereznwo.github.io'
                    ,'django-setup-windows.onrender.com'
                    ,'django-setup-windows-latest-9vkx.onrender.com' 
+                   ,'django-setup-windows-latest-e1ed.onrender.com'
                    ]
 
 # Application definition
